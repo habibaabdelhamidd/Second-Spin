dd26f6ee93df703d16b255652013dd93c151edc2
Kholoudd <kholoudemad551@gmail.com>
2024-04-25 18:16:46 +0200

    onboarding done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:graduation/layouts/homelayout/homelayout.dart';
 import 'package:graduation/screens/boarding/boarding1.dart';
+import 'package:graduation/screens/boarding/boarding2.dart';
+import 'package:graduation/screens/boarding/onboarding3.dart';
+import 'package:graduation/screens/splash/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +17,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: Onboarding1.routeName,
       routes: {
+        // Splashscreen.routeName :(context)=> Splashscreen(),
         Onboarding1.routeName : (context)=> Onboarding1() ,
-      }
-
+        Onboarding2.routeName: (context)=> Onboarding2(),
+        Onboarding3.routeName: (context)=> Onboarding3(),
+        // HomeLayout.routeName : (context)=> HomeLayout(),
+      },
+      home: Onboarding1(),
     );
   }
 }
--- lib/screens/boarding/boarding1.dart
+++ lib/screens/boarding/boarding1.dart
@@ -1,83 +1,90 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:graduation/screens/boarding/boarding2.dart';
 
 class Onboarding1 extends StatelessWidget {
   static const String routeName = "onboarding1";
 
   @override
   Widget build(BuildContext context) {
-    final mediaquary = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Image.asset("assets/image/On boarding 01.png"),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: 320,
-                  ),
-                  Container(
-                    child: Text(
-                      "Skip",
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0XFF757575)),
+    return Container(
+      child: Stack(
+        children: [
+          Image.asset("assets/image/On boarding 01.png"),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 320,
                     ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  Image.asset("assets/image/bording1.png"),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: Text(
-                      "The best simple place where you "
-                      "discover most wonderful used and new Products.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0XFF2B3139),
-                          fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 300,
-                  ),
-                  Container(
-                    width: 70,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        color: Color(0xff7AAB99),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          right: 8.0, left: 8, bottom: 8, top: 12),
+                    Container(
                       child: Text(
-                        "Next",
+                        "Skip",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0XFF757575)),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("assets/image/bording1.png"),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Text(
+                        "The best simple place where you "
+                        "discover most wonderful used and new Products.",
+                        textAlign: TextAlign.center,
                         style: TextStyle(
                             fontSize: 20,
-                            color: Color(0XFFffffff),
+                            color: Color(0XFF2B3139),
                             fontWeight: FontWeight.w500),
-                        textAlign: TextAlign.center,
                       ),
                     ),
-                  )
-                ],
-              ),
-            ],
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 300,
+                    ),
+                    Container(
+                      width: 70,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Color(0xff7AAB99),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            right: 8.0, left: 8, bottom: 8, top: 12),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context,Onboarding2.routeName,);
+                          },
+                          child: Text(
+                            "Next",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0XFFffffff),
+                                fontWeight: FontWeight.w500),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
