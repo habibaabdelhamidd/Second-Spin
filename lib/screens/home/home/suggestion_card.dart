26f6a25c70bfaf50c6066f236fbb81c7d0d301ff
Kholoudd <kholoudemad551@gmail.com>
2024-05-14 19:22:45 +0300

    edit

---
+++ lib/screens/home/home/suggestion_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Suggestions_Card extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final mediaquary = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+    return Container(
+      padding: EdgeInsets.all(8),
+      width:mediaquary.width/2.6,
+      decoration: BoxDecoration(
+        color:Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment:CrossAxisAlignment.start,
+        children: [
+          Image.asset("assets/image/black_lap.png" , fit: BoxFit.fill,),
+          Text("Apple MacBook Pro 13" , style: theme.textTheme.bodyLarge,),
+          Text("16,500 EG" , style: theme.textTheme.bodyLarge,),
+          Text("Madinaty, Egypt" , style: theme.textTheme.bodyMedium),
+          Row(children: [
+            SizedBox(width: mediaquary.width/3.5,),
+            ImageIcon(AssetImage("assets/image/Icon fav.png" ,),color: theme.primaryColor,)
+          ],),
+        ],
+      ),
+    );
+  }
+
+}
