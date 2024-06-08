fce2f1cb6fd2d7b3e1045b0e6f8a05bfd00385de
Kholoudd <kholoudemad551@gmail.com>
2024-06-08 14:45:42 +0300

    edit

--- lib/core/network_layer/api_manager.dart
+++ lib/core/network_layer/api_manager.dart
@@ -10,10 +10,6 @@ import 'package:http/http.dart' as http;
 class Api_Manager {
   Future<List<Data>?> fetchHome() async {
     final response = await http.get(
-      Uri.parse(
-        Uri.http(
-          Constants.api_base_URL ,
-          "/api/products/home",
         Uri.http(
           Constants.api_base_URL ,
           "/api/products/home",
@@ -21,13 +17,7 @@ class Api_Manager {
       headers:{
         "Authorization":"Bearer 7|Mg31lmlgv4yc0EcWuwYsb1lYGP1bV1XVnEae6Z5f25d6b3dd"
       }
-        Uri.parse(
-          // Constants.Ai_base_URLp + "/api/products/home",
-            "http://10.0.2.2:8000/api/products/home"
-        ),
-      headers:{
-        "Authorization":"Bearer 7|Mg31lmlgv4yc0EcWuwYsb1lYGP1bV1XVnEae6Z5f25d6b3dd"
-      };
+    );
     final decodedResponse = jsonDecode(response.body);
     if (response.statusCode == 200 && decodedResponse["status"] == true) {
       final homemodel = HomeModel.fromJson(decodedResponse);
